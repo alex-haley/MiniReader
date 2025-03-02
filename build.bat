c56fca6984f0d156bb76c5c87b6c0a1b225fd197
alex-haley <mloyygraduate7@gmail.com>
2025-03-02 15:27:07 +0800

    update debug tools

--- build.bat
+++ build.bat
@@ -1,7 +1,18 @@
 @echo off
 
+IF "%1"=="-compile" goto compile
+
 pushd build
-echo "debug compilation"
+echo "debug build"
 javac -cp "../lib/pdfbox.jar;../src/Main.java;../src/PDFViewer.java;" -d ./ ../src/*.java
 java -cp "../lib/pdfbox.jar;../src/Main.java;../src/PDFViewer.java;" Main ../books/%1
+popd
+
+goto :eof
+
+:compile
+
+pushd build
+echo "compiling"
+javac -cp "../lib/pdfbox.jar;../src/Main.java;../src/PDFViewer.java;" -d ./ ../src/*.java
 popd
